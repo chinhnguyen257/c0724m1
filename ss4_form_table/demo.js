459690a2317fca028e5abd400e292a2860f3e98f
chinhnguyen257 <chinhkon0308@gmail.com>
2024-08-28 19:07:17 +0700

    chinh them moi ss3

---
+++ ss4_form_table/demo.js
@@ -0,0 +1,5 @@
+function clickMe() {
+    //alert("bạn đã like bài viết")
+    document.getElementById("like").style.backgroundColor="Blue";
+    document.getElementById("like").style.Color="White";
+}
